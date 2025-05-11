660eee91324b48536ced1e97d49e058d61d61271
Om Thanage <om.thanage@somaiya.edu>
2025-05-11 16:40:17 +0530

    feat: Add microsoft clarity details

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import {  RouterProvider } from "react-router-dom";
 import { ToastProvider } from "./context/ToastContext";
 import "./App.css";
 import router from "./configs/router";
+import { clarity } from 'react-microsoft-clarity'
+
 
 function App() {
+  clarity.init('rhuukpshtr')
   return (
     <ToastProvider>
       <RouterProvider router={router} />
