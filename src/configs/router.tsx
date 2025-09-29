f914a32a786227a9d73784a6dddbc34c1511b2a5
Om-Thanage <omsthanage2005@gmail.com>
2025-09-29 19:04:53 +0530

    feat : Form closed

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -5,7 +5,7 @@ import Thankyou from "../pages/Thankyou";
 import Admin from "../pages/Admin";
 import Preview from "../pages/Preview";
 import Formclosed from "../pages/Formclosed";
-import Form from "../pages/Form";
+// import Form from "../pages/Form";
 
 const router = createBrowserRouter([
     {
@@ -24,14 +24,14 @@ const router = createBrowserRouter([
             </> 
         )
     },
-    {
-        path: '/form',
-        element: (
-            <>
-                <Form/>
-            </>
-        )
-    },
+    // {
+    //     path: '/form',
+    //     element: (
+    //         <>
+    //             <Form/>
+    //         </>
+    //     )
+    // },
     {
         path:"/success",
         element: (
